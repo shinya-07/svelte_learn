32adbe3bb9716724ae453c689b12c30bbe4727a1
shinya-07 <19c1080027ms@edu.tech.ac.jp>
2022-05-27 18:04:27 +0900

    1st commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-red-600">Home</h1>
+<p>motowaki shinya</p>
+<h1>藏田</h1>
+<a href="http://game.granbluefantasy.jp/#loginbonus/loginbonus_usual_008" target="_blank" class="text-9xl text-lime-700">新井</a><br>
+<a href="/page2">page2</a>
---
+++ src/routes/page2.svelte
@@ -0,0 +1,2 @@
+<h1>page2</h1>
+<a href="/">Homeに戻る</a>
